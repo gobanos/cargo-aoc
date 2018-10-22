42522c2734fd7700398ce7beea93b8ff5b37eb8b
Grégory Obanos <gregory.obanos@gmail.com>
2018-10-22 12:41:13 +0200

    Macro !

--- src/lib.rs
+++ src/lib.rs
@@ -1,70 +1,110 @@
-extern crate reqwest;
-use reqwest::Client;
-
-use std::time::{Instant, Duration};
-use std::marker::PhantomData;
+use std::fmt::Display;
+use std::sync::Arc;
 
 #[inline]
-pub fn identity<T>(t: T) -> T { t }
-
-const COOKIE: &str = "session=53616c7465645f5f5be86546add1d74265114cd6b3617545ba301f4df94136496d32bcfd2386a5ad82ed44dc1ab1d052";
-
-pub struct Generator<I, G, F, R, A> where G: Fn(String) -> I, F: Fn(&A) -> R, I: AsRef<A>, A: ?Sized{
-    day: u8,
-    generator: G,
-    processor: F,
-    _marker_a: PhantomData<A>,
+pub fn identity<T>(t: T) -> T {
+    t
 }
 
-impl<I, G, F, R, A> Generator<I, G, F, R, A> where G: Fn(String) -> I, F: Fn(&A) -> R, I: AsRef<A>, A: ?Sized {
-    pub fn new(day: u8, generator: G, processor: F) -> Generator<I, G, F, R, A> {
-        Generator {
-            day,
-            generator,
-            processor,
-            _marker_a: PhantomData,
-        }
-    }
+pub struct ArcStr(Arc<str>);
 
-    pub fn generate(self) -> Runner<I, F, R, A> {
-        let input_str = Client::new().get(&format!("https://adventofcode.com/2015/day/{}/input", self.day))
-            .header("Cookie", COOKIE)
-            .send()
-            .unwrap()
-            .text()
-            .unwrap();
-
-        let start_time = Instant::now();
-        let input = (self.generator)(input_str);
-        let duration = Instant::now() - start_time;
-
-        Runner {
-            day: self.day,
-            input,
-            processor: self.processor,
-            generator_duration: duration,
-            _marker_a: PhantomData,
-        }
-    }
-}
-
-impl<F, R, A> Generator<String, fn(String) -> String, F, R, A> where F: Fn(&A) -> R, String: AsRef<A>, A: ?Sized {
-    pub fn simple(day: u8, processor: F) -> Generator<String, fn(String) -> String, F, R, A> {
-        Generator::new(day, identity, processor)
-    }
-}
-
-pub struct Runner<I, F, R, A> where F: Fn(&A) -> R, I: AsRef<A>, A: ?Sized {
-    day: u8,
-    input: I,
-    processor: F,
-    generator_duration: Duration,
-    _marker_a: PhantomData<A>,
-}
-
-impl<I, F, R, A> Runner<I, F, R, A> where F: Fn(&A) -> R, I: AsRef<A>, A: ?Sized {
+impl ArcStr {
     #[inline]
-    pub fn run(&self) -> R {
-        (self.processor)(self.input.as_ref())
+    pub fn from(f: &str) -> ArcStr {
+        ArcStr(Arc::from(f))
     }
+}
+
+impl AsRef<str> for ArcStr {
+    fn as_ref(&self) -> &str {
+        self.0.as_ref()
+    }
+}
+
+pub trait Runner
+where
+    Self::Result: Display,
+{
+    type Result;
+
+    fn gen(input: ArcStr) -> Self;
+    fn run(&self) -> Self::Result;
+}
+
+#[macro_export]
+macro_rules! aoc {
+    ( @as_meta, $m:meta ) => { $m };
+    ( @generator, pub fn $name:ident ( $arg:ident : $arg_t:ty ) -> $out_t:tt $body:block ) => {
+        pub fn $name($arg: $arg_t) -> $out_t {
+            $body
+        }
+    };
+    ( @runner #[ aoc( $day:meta , $part:meta ) ] pub fn $name:ident ( $arg:ident : $arg_t:ty ) -> $out_t:ty $body:block ) => {
+        pub use self::$name::runner as $name;
+
+        #[allow(unused_imports)]
+        mod $name {
+            use super::*;
+            use aoc_runner::{ArcStr, Runner};
+            use std::marker::PhantomData;
+
+            pub fn runner($arg: $arg_t) -> $out_t {
+                $body
+            }
+
+            #[derive(Runner)]
+            #[runner(runner)]
+            pub struct RunnerStruct {
+                input: ArcStr,
+                output: PhantomData<$out_t>,
+            }
+        }
+    };
+    ( @runner_gen $gen_name:ident -> $gen_t:ty , #[ aoc( $day:meta , $part:meta ) ] pub fn $name:ident ( $arg:ident : $arg_t:ty ) -> $out_t:ty $body:block ) => {
+        pub use self::$name::runner as $name;
+
+        #[allow(unused_imports)]
+        mod $name {
+            use super::*;
+            use aoc_runner::{ArcStr, Runner};
+            use std::marker::PhantomData;
+
+            pub fn runner($arg: $arg_t) -> $out_t {
+                $body
+            }
+
+            #[derive(Runner)]
+            #[runner(runner, $gen_name)]
+            pub struct RunnerStruct {
+                input: $gen_t,
+                output: PhantomData<$out_t>,
+            }
+        }
+    };
+    ( #[ aoc( $day:meta , $part:meta ) ] pub fn $name:ident ( $arg:ident : $arg_t:ty ) -> $out_t:ty $body:block ) => {
+        aoc! { @runner #[aoc($day, $part)] pub fn $name ($arg: $arg_t) -> $out_t { $body } }
+    };
+    (
+     #[ aoc( $a_day:meta , $a_part:meta ) ] pub fn $a_name:ident ( $a_arg:ident : $a_arg_t:ty ) -> $a_out_t:ty $a_body:block
+     #[ aoc( $b_day:meta , $b_part:meta ) ] pub fn $b_name:ident ( $b_arg:ident : $b_arg_t:ty ) -> $b_out_t:ty $b_body:block
+    ) => {
+        aoc! { @runner #[aoc($a_day, $a_part)] pub fn $a_name ($a_arg: $a_arg_t) -> $a_out_t { $a_body } }
+        aoc! { @runner #[aoc($b_day, $b_part)] pub fn $b_name ($b_arg: $b_arg_t) -> $b_out_t { $b_body } }
+    };
+    (
+     #[generator] pub fn $gen_name:ident ( $gen_arg:ident : $gen_arg_t:ty ) -> $gen_out_t:ty $gen_body:block
+     #[ aoc( $day:meta , $part:meta ) ] pub fn $name:ident ( $arg:ident : $arg_t:ty ) -> $out_t:ty $body:block
+    ) => {
+        aoc! { @generator, pub fn $gen_name($gen_arg: $gen_arg_t) -> $gen_out_t { $gen_body } }
+        aoc! { @runner_gen $gen_name -> $gen_out_t, #[aoc($day, $part)] pub fn $name ($arg: $arg_t) -> $out_t { $body } }
+    };
+    (
+     #[generator] pub fn $gen_name:ident ( $gen_arg:ident : $gen_arg_t:ty ) -> $gen_out_t:ty $gen_body:block
+     #[ aoc( $a_day:meta , $a_part:meta ) ] pub fn $a_name:ident ( $a_arg:ident : $a_arg_t:ty ) -> $a_out_t:ty $a_body:block
+     #[ aoc( $b_day:meta , $b_part:meta ) ] pub fn $b_name:ident ( $b_arg:ident : $b_arg_t:ty ) -> $b_out_t:ty $b_body:block
+    ) => {
+        aoc! { @generator, pub fn $gen_name($gen_arg: $gen_arg_t) -> $gen_out_t { $gen_body } }
+        aoc! { @runner_gen $gen_name -> $gen_out_t, #[aoc($a_day, $a_part)] pub fn $a_name ($a_arg: $a_arg_t) -> $a_out_t { $a_body } }
+        aoc! { @runner_gen $gen_name -> $gen_out_t, #[aoc($b_day, $b_part)] pub fn $b_name ($b_arg: $b_arg_t) -> $b_out_t { $b_body } }
+    };
 }
