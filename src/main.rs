bce3940f5a82a7f0054e6fc26ed379e127ac89cf
Olivier Pinon <oliv.pinon@gmail.com>
2018-11-29 17:57:10 +0100

    fixed cargo-aoc working and cargo aoc not working + simplified main code a bit (pattern matching)

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,11 @@ use app::AOCApp;
 
 fn main() {
     // Parses
-    let matches = App::new("aoc")
+    let matches = App::new("cargo-aoc")
         .version("1.0")
         .about("Cargo helper for Advent of Code")
         .author("gobanos <gregory.obanos@gmail.com>")
+        .arg(Arg::with_name("dummy").hidden(true).possible_value("aoc"))
         .subcommand(
             SubCommand::with_name("credentials")
                 .about("Manage your AOC credentials information")
@@ -31,7 +32,8 @@ fn main() {
                         .help("Sets the session cookie")
                         .takes_value(true),
                 ),
-        ).subcommand(
+        )
+        .subcommand(
             SubCommand::with_name("input")
                 .about("Get the input for a specified date")
                 .arg(
@@ -39,29 +41,23 @@ fn main() {
                         .short("d")
                         .help("Specifies the day. Defaults to today's date.")
                         .takes_value(true),
-                ).arg(
+                )
+                .arg(
                     Arg::with_name("year")
                         .short("y")
                         .help("Specifies the year. Defaults to the current year.")
                         .takes_value(true),
                 ),
-        ).get_matches();
+        )
+        .get_matches();
 
     // Creates an AOCApp that we'll use to launch actions (commands)
     let app = AOCApp::new();
 
-    // If subcommand "credentials" is called
-    if let Some(args) = matches.subcommand_matches("credentials") {
-        app.execute_credentials(args);
-        return;
+    match matches.subcommand() {
+        ("credentials", Some(m)) => app.execute_credentials(&m),
+        ("input", Some(m)) => app.execute_input(&m),
+        (_, Some(_)) => panic!("Unknown command"),
+        _ => app.execute_default().unwrap(),
     }
-
-    // If subcommand "input" is called
-    if let Some(args) = matches.subcommand_matches("input") {
-        app.execute_input(args);
-        return;
-    }
-
-    // Else, let's use the default command and let the runner do the rest
-    app.execute_default().unwrap();
 }
