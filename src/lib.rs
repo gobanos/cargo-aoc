15c0f79b963cde0c02016a968ed553787dd2ce93
Grégory Obanos <gregory.obanos@gmail.com>
2018-11-30 19:58:43 +0100

    Add doc

--- src/lib.rs
+++ src/lib.rs
@@ -28,21 +28,64 @@ pub fn aoc_runner_derive(input: pm::TokenStream) -> pm::TokenStream {
 }
 
 #[proc_macro_attribute]
+/// # Solution meta
+///
+/// Use this to flag a function as a solution for a given day :
+/// `#[aoc(day1, part1)]`
+///
+/// You can also add a custom name to the function :
+/// `#[aoc(day1, part1, Bytes)]`, it's useful to have multiple solutions to a given day & part and compare them !
+///
+/// The function must take a single parameter : a `&str` or a `&[u8]`, unless you use a [generator]
+/// and return any type implementing `Display`.
+///
+/// [generator]: attr.aoc_generator.html
 pub fn aoc(args: pm::TokenStream, input: pm::TokenStream) -> pm::TokenStream {
     runner::runner_impl(args, input)
 }
 
 #[proc_macro_attribute]
+/// # Generator meta
+///
+/// Use a generator when you need to pre-process your input :
+///
+/// ## Usage
+/// Generator meta have 3 forms :
+///  - a generator for the whole day : `#[aoc_generator(day1)]`
+///  - a generator for a single part : `#[aoc_generator(day1, part1)]`
+///  - a generator for a single (named) solution: `#[aoc_generator(day1, part1, Bytes)]`
+///
+/// The function must take a single parameter : a `&str` or a `&[u8]`, and output any sized type.
+///
+/// The corresponding solutions now take any parameter for which `AsRef` is implemented.
+///
+/// ## Note
+/// A generator must be declared before it's solutions.
+///
 pub fn aoc_generator(args: pm::TokenStream, input: pm::TokenStream) -> pm::TokenStream {
     generator::generator_impl(args, input)
 }
 
 #[proc_macro]
+/// # Library declaration
+///
+/// This macro must be at the end of lib.rs
+///
+/// ## Usage
+/// `aoc_lib! { year = 2018 }`
 pub fn aoc_lib(input: pm::TokenStream) -> pm::TokenStream {
     out::lib_impl(input)
 }
 
 #[proc_macro]
+/// # Main declaration
+///
+/// This macro must be at the end of main.rs
+///
+/// ## Usage
+/// `aoc_main` has 2 forms :
+///  - as a standalone binary : `aoc_main! { year = 2018 }`
+///  - as a link to a library : `aoc_main! { lib = advent_of_code_2018 }` (you must had `extern crate advent_of_code_2018;` before)
 pub fn aoc_main(input: pm::TokenStream) -> pm::TokenStream {
     out::main_impl(input)
 }
