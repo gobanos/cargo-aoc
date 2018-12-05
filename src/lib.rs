cf2195a398f2f8dc195004480d6ce156c9778c09
Grégory Obanos <gregory.obanos@gmail.com>
2018-12-05 23:46:34 +0100

    Version 0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,19 @@
+use std::error::Error;
 use std::fmt::Display;
 use std::sync::Arc;
-use std::error::Error;
 
 #[inline]
 pub fn identity<T>(t: T) -> T {
     t
 }
 
+#[derive(Clone, Debug)]
 pub struct ArcStr(Arc<str>);
 
 impl ArcStr {
     #[inline]
     pub fn from(f: &str) -> ArcStr {
-        ArcStr(Arc::from(f))
+        ArcStr(Arc::from(f.trim()))
     }
 }
 
@@ -28,13 +29,28 @@ impl AsRef<[u8]> for ArcStr {
     }
 }
 
+impl AsRef<Arc<str>> for ArcStr {
+    fn as_ref(&self) -> &Arc<str> {
+        &self.0
+    }
+}
+
 pub trait Runner {
-    fn gen(input: ArcStr) -> Self where Self: Sized;
-    fn try_gen(input: ArcStr) -> Result<Self, Box<dyn Error>> where Self: Sized {
+    fn gen(input: ArcStr) -> Self
+    where
+        Self: Sized;
+
+    fn run(&self) -> Box<dyn Display>;
+
+    fn bench(&self, black_box: fn(&dyn Display));
+
+    fn try_gen(input: ArcStr) -> Result<Self, Box<dyn Error>>
+    where
+        Self: Sized,
+    {
         Ok(Self::gen(input))
     }
 
-    fn run(&self) -> Box<dyn Display>;
     fn try_run(&self) -> Result<Box<dyn Display>, Box<dyn Error>> {
         Ok(self.run())
     }
